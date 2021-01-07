f867f4183c317561570c65a78d9eecda65e5e5a8
Eduardo Rosales Fernández <eduardo900516@gmail.com>
2021-01-07 02:11:59 -0300

    first commit

---
+++ src/test/java/pa/diners/tc/NativeTarjetaCreditoResourceIT.java
@@ -0,0 +1,9 @@
+package pa.diners.tc;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTarjetaCreditoResourceIT extends TarjetaCreditoResourceTest {
+
+    // Execute the same tests but in native mode.
+}
